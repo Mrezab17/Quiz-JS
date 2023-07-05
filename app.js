a7baf87e7f1144fc3f422174a627fe791dd35f5b
mreza <mrezabmiri17@gmail.com>
2023-07-05 23:50:29 +0330

    navigation bar finished .

---
+++ app.js
@@ -0,0 +1,16 @@
+const goToBanner = () => {
+    window.location.href = '#banner';
+    console.log("Hey");
+}
+const goToCoinList = () => {
+    window.location.href = '#coinlist';
+    console.log("Hey");
+}
+const goToConvert = () => {
+    window.location.href = '#convertbox';
+    console.log("Hey");
+}
+const goToFooter = () => {
+    window.location.href = '#footer';
+    console.log("Hey");
+}
